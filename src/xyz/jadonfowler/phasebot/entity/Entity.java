f21d9b369165531de4b7a313ed28000c9f94e734
phase <ph4sesaber@gmail.com>
2015-05-31 06:50:54 -0700

    Lombok hoooo

--- src/xyz/jadonfowler/phasebot/entity/Entity.java
+++ src/xyz/jadonfowler/phasebot/entity/Entity.java
@@ -2,14 +2,17 @@ package xyz.jadonfowler.phasebot.entity;
 
 import java.util.HashMap;
 
+import lombok.*;
+
+@ToString(exclude={"pitch", "yaw"})
 public class Entity {
 	
-	public static HashMap<Integer, Entity> entities = new HashMap<Integer, Entity>();
+	@Getter private static HashMap<Integer, Entity> entities = new HashMap<Integer, Entity>();
 	
-	public int entityId;
-	public String type;
-	public double x, y, z;
-	public float yaw = 0, pitch = 0;
+	@Getter private int entityId;
+	@Getter private String type;
+	@Getter @Setter public double x, y, z;
+	@Getter @Setter public float yaw = 0, pitch = 0;
 	
 	public Entity(int i, String type, double x, double y, double z, float yaw, float pitch){
 		this.entityId = i;
@@ -28,10 +31,6 @@ public class Entity {
 	
 	public void remove(){
 		entities.remove(this.entityId);
-	}
-	
-	public String toString(){
-		return "Entity[entityId=" + entityId + ",type=" + type + ",x=" + x + ",y=" + y + ",z=" + z + "]";
 	}
 	
 	public static Entity byId(int id){
