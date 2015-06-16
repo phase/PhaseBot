ec33592469eae583887da7b1af50a3b27621e119
phase <ph4sesaber@gmail.com>
2015-06-16 00:13:13 -0700

    connection test

---
+++ src/xyz/jadonfowler/phasebot/test/ConnectionTest.java
@@ -0,0 +1,12 @@
+package xyz.jadonfowler.phasebot.test;
+
+import org.junit.Test;
+import xyz.jadonfowler.phasebot.PhaseBot;
+
+public class ConnectionTest {
+    
+    @Test
+    public void connect(){
+        PhaseBot.status();
+    }
+}
